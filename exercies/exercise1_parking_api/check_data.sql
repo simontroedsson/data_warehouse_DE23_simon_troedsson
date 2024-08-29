a7f4e37028e1024782c5e9a9276d4c0b33f8ef82
simon <simon.troedsson94@gmail.com>
2024-08-29 16:38:19 +0200

    added glossary

--- exercies/exercise1_parking_api/check_data.sql
+++ exercies/exercise1_parking_api/check_data.sql
@@ -1,6 +1,12 @@
 
 USE ROLE PARKING_INFO_DLT_ROLE;
+
+USE DATABASE parkingdb;
+
+use schema parkingdb.staging;
 SHOW TABLES IN SCHEMA parkingdb.staging;
 
 
 SELECT * FROM data_field_parking limit 5;
+
+SELECT * FROM DATA_FIELD_PARKING__BBOX LIMIT 5;
